476ef9d4d717c80fb9fcf4b42fd96fb948ba0ca6
Zoran Vasilic <48178249+zokica8@users.noreply.github.com>
2019-12-09 16:31:05 +0100

    Small Mockito test added.

--- app/src/test/java/com/nsweb/heroapp/ExampleUnitTest.java
+++ app/src/test/java/com/nsweb/heroapp/ExampleUnitTest.java
@@ -1,9 +1,14 @@
 package com.nsweb.heroapp;
 
 
-import org.junit.jupiter.api.Test;
+import com.nsweb.heroapp.data.repositories.SuperHeroRepository;
 
-import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +16,16 @@ import static org.assertj.core.api.Assertions.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 // This is an example of a Junit5 test
+@ExtendWith(MockitoExtension.class)
 class ExampleUnitTest {
+
+    @Mock
+    private SuperHeroRepository superHeroRepository;
+
+    @Test
+    void shouldTestMock() {
+        assertThat(superHeroRepository).isNotNull();
+    }
 
     @Test
     void testTrueIsTrue() {
